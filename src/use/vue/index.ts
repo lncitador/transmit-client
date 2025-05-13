51f0031a22c8fbe6fb1675a7d15d2d61f02174c4
Walaff Fernandes <walaffonofre@icloud.com>
2025-05-12 21:30:00 -0300

    feat: reorganize exports in Vue index file to include type exports

--- src/use/vue/index.ts
+++ src/use/vue/index.ts
@@ -1,2 +1,4 @@
-export { useTransmit } from './hook.js'
+export type { UseTransmitEvent, UseTransmitOptions } from '../types.js'
+export type { UseTransmitReturn, UseTransmitDataUnwrapRef as UseTransmitData } from './hook.js'
 export { TransmitPlugin } from './plugin.js'
+export { useTransmit } from './hook.js'
